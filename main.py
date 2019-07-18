568450c74788051e19998ac7d26638b5938789cc
SupaHotFiya27 <ameyers@andrew.cmu.edu>
2019-07-17 20:11:55 -0400

    added objective functions to attempt to minimize distance between nodes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from circle import *
 from z3 import *
 
 circles = detect_circles("input.png", "output.png")
-s = Solver()
+s = Optimize()
 n = len(circles)
 xcoords = []
 ycoords = []
@@ -14,8 +14,8 @@ for x in range(0, n):
 for y in range(0, n):
     ycoords.append(Int("y_" + str(y)))
     s.add(ycoords[y] >= 0)
-s.add(Sum(xcoords) < n ** 2)
-s.add(Sum(ycoords) < n ** 2)
+#s.add(Sum(xcoords) < n ** 2)
+#s.add(Sum(ycoords) < n ** 2)
 for i in range(0, n):
     for j in range(i + 1, n):
         if(abs(circles[i][0] - circles[j][0]) < circles[i][2]):
@@ -30,5 +30,12 @@ for i in range(0, n):
             s.add(ycoords[i] < ycoords[j])
         else:
             s.add(ycoords[i] > ycoords[j])
+for i in range(0, n):
+    for j in range(i + 1, n):
+        s.minimize(xcoords[i] - xcoords[j])
+        s.minimize(xcoords[j] - xcoords[i])
+        s.minimize(ycoords[i] - ycoords[j])
+        s.minimize(ycoords[j] - ycoords[i])
 if(s.check() == sat):
     print(s.model())
+#    print(s.objectives())
